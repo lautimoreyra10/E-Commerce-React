61a77f0f13ab93248403e051b6bf6cdb9978871c
lautimoreyra10 <lauti_15@hotmail.es>
2025-01-10 18:14:51 -0300

    navigate al login

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,34 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import jwt_decode from 'jwt-decode';
+import { useNavigate } from 'react-router-dom';
 import { useAuth0 } from "@auth0/auth0-react";
 
 interface UserData {
-  firstName: string;
-  lastName: string;
-  email: string;
-  address: string;
-  document: string;
-  card: string;
-  imageUrl: string;
-  role: string;
+  id?: number;
+  name?: string;
+  email?: string;
+  roleId?: number;
+  roleName?: string;
 }
 
 const ProfilePage: React.FC = () => {
   const { user, isAuthenticated, isLoading, logout } = useAuth0();
-  const [userData, setUserData] = useState<UserData>({
-    firstName: '',
-    lastName: '',
-    email: '',
-    address: '',
-    document: '',
-    card: '',
-    imageUrl: '',
-    role: '',
-  });
+  const [userData, setUserData] = useState<UserData | null>({name: '', email: '', id: 1});
   const [loading, setLoading] = useState<boolean>(true);
   const [message, setMessage] = useState<string>('');
+  
+  // Usa navigate para redirigir al usuario cuando sea necesario
+  const navigate = useNavigate();
 
-  // Simula cargar los datos adicionales del usuario desde tu API
   const getUserProfile = async () => {
     const token = localStorage.getItem('token');
     if (!token) return;
@@ -40,21 +30,25 @@ const ProfilePage: React.FC = () => {
         },
       });
       if (!res.ok) throw new Error('Error al obtener el perfil del usuario');
-      const data = await res.json();
-      setUserData(data);
+      
+      const { userList } = await res.json();
+      const matchedUser = userList.find((u: UserData) => u.email === user?.email);
+
+      if (matchedUser) {
+        setUserData(matchedUser);
+      } else {
+        throw new Error('Usuario no encontrado en la base de datos');
+      }
     } catch (err) {
       console.error(err);
       localStorage.removeItem('token');
+      setMessage('Error al cargar el perfil');
+      navigate('/login'); // Navegar a login en caso de error
     }
   };
 
   useEffect(() => {
     if (isAuthenticated) {
-      setUserData({
-        ...userData,
-        email: user?.email || '',
-        imageUrl: user?.picture || '',
-      });
       getUserProfile();
       setLoading(false);
     }
@@ -67,7 +61,7 @@ const ProfilePage: React.FC = () => {
 
     try {
       const token = localStorage.getItem('token');
-      const res = await fetch('http://localhost:5000/api/users/profile', {
+      const res = await fetch(`https://commercial-api.vulktech.com/users/${userData?.id}`, {
         method: 'PUT',
         headers: { Authorization: `Bearer ${token}` },
         body: formData,
@@ -76,7 +70,7 @@ const ProfilePage: React.FC = () => {
       if (res.ok) {
         setMessage('Perfil actualizado con éxito');
       } else {
-        setMessage(`Error al actualizar el perfil: ${await res.text()}`);
+        navigate('/login');
       }
     } catch (error) {
       setMessage('Error en la conexión');
@@ -91,103 +85,53 @@ const ProfilePage: React.FC = () => {
     <div className="max-w-2xl mx-auto p-6 bg-gray-100 rounded-lg shadow-lg">
       <h1 className="text-center text-2xl mb-5">Perfil del Usuario</h1>
       {isAuthenticated ? (
-        <form className="grid grid-cols-2 gap-5" id="profile-form" onSubmit={handleSaveChanges}>
-          <div className="flex flex-col mb-3">
-            <label className="text-sm mb-1 text-gray-700">Nombre</label>
-            <input
-              type="text"
-              name="firstName"
-              value={userData.firstName}
-              className="form-input"
-              placeholder="Nombre"
-              onChange={(e) => setUserData({ ...userData, firstName: e.target.value })}
-            />
-          </div>
-
-          <div className="flex flex-col mb-3">
-            <label className="text-sm mb-1 text-gray-700">Apellido</label>
-            <input
-              type="text"
-              name="lastName"
-              value={userData.lastName}
-              className="form-input"
-              placeholder="Apellido"
-              onChange={(e) => setUserData({ ...userData, lastName: e.target.value })}
-            />
-          </div>
-
-          <div className="flex flex-col mb-3">
-            <label className="text-sm mb-1 text-gray-700">Correo Electrónico</label>
-            <input
-              type="email"
-              name="email"
-              value={userData.email}
-              className="form-input"
-              placeholder="Correo Electrónico"
-              disabled
-            />
-          </div>
-
-          <div className="flex flex-col mb-3">
-            <label className="text-sm mb-1 text-gray-700">Dirección</label>
-            <input
-              type="text"
-              name="address"
-              value={userData.address}
-              className="form-input"
-              placeholder="Dirección"
-              onChange={(e) => setUserData({ ...userData, address: e.target.value })}
-            />
-          </div>
-
-          <div className="flex flex-col mb-3">
-            <label className="text-sm mb-1 text-gray-700">Documento</label>
-            <input
-              type="text"
-              name="document"
-              value={userData.document}
-              className="form-input"
-              placeholder="Documento de Identidad"
-              onChange={(e) => setUserData({ ...userData, document: e.target.value })}
-            />
-          </div>
-
-          <div className="flex flex-col mb-3">
-            <label className="text-sm mb-1 text-gray-700">Tarjeta</label>
-            <input
-              type="text"
-              name="card"
-              value={userData.card}
-              className="form-input"
-              placeholder="Número de Tarjeta"
-              onChange={(e) => setUserData({ ...userData, card: e.target.value })}
-            />
-          </div>
-
-          <div className="flex flex-col mb-3">
-            <label className="text-sm mb-1 text-gray-700">URL de la Imagen de Perfil</label>
-            <input
-              type="text"
-              name="imageUrl"
-              value={userData.imageUrl}
-              className="form-input"
-              placeholder="URL de Imagen de Perfil"
-              onChange={(e) => setUserData({ ...userData, imageUrl: e.target.value })}
-            />
-            {userData.imageUrl && (
-              <img
-                src={userData.imageUrl}
-                alt="Foto de Perfil"
-                className="profile-image"
+        userData ? (
+          <form className="grid grid-cols-2 gap-5" id="profile-form" onSubmit={handleSaveChanges}>
+            <div className="flex flex-col mb-3">
+              <label className="text-sm mb-1 text-gray-700">Nombre</label>
+              <input
+                type="text"
+                name="name"
+                value={userData.name}
+                className="form-input"
+                placeholder="Nombre"
+                disabled
               />
-            )}
-          </div>
+            </div>
+
+            <div className="flex flex-col mb-3">
+              <label className="text-sm mb-1 text-gray-700">Correo Electrónico</label>
+              <input
+                type="email"
+                name="email"
+                value={userData.email}
+                className="form-input"
+                placeholder="Correo Electrónico"
+                disabled
+              />
+            </div>
+
+            <div className="flex flex-col mb-3">
+              <label className="text-sm mb-1 text-gray-700">Rol</label>
+              <input
+                type="text"
+                name="roleName"
+                value={userData.roleName}
+                className="form-input"
+                disabled
+              />
+            </div>
+
             <div className='flex flex-col mb-3' id="button-container">
               <button type="submit" className="save-button hover:text-green-500" id="submit-btn">
                 Guardar Cambios
               </button>
             </div>
-        </form>
+          </form>
+        ) : (
+          <p>No se encontró el usuario.</p>
+
+        )
       ) : (
         <p>No estás autenticado.</p>
       )}
